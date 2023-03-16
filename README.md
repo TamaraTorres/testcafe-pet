73920929e605a9611343987a3f92e42c1fd09034
Tamara Torres <tammy.torres.88@gmail.com>
2023-03-16 16:24:51 -0300

    adding changes

--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 Pet Proyect using Ubuntu + testcafe 
 
 ## Requirements 
+* npm https://www.npmjs.com/ 
 * node +14 -https://nodejs.org/en/download/releases/ 
 * testcafe  https://testcafe.io/documentation/402834/guides/basic-guides/install-testcafe
 * nvm (optional to handle node versions) https://github.com/nvm-sh/nvm 
+
+## To run the test 
+On a terminal run :
+* `npm i -g testcafe` // will install testcafe from npm 
+* `testcafe chrome first_test.js` // this will only run the test on chrome browser
+* `testcafe chrome:headless first_test.js` // headless mode 
+
+## About the tests 
+Simple scenarios using testcafe test page https://devexpress.github.io/testcafe/example/ 
+Clicking radio buttons, checkbox, buttons, skiping test, etc
--- first_test.js
+++ first_test.js
@@ -10,14 +10,19 @@ fixture('Getting Started')
     });
 
 test('My first test', async t => {
+	const developer_name = Selector('#developer-name')
+	const linux_os= Selector('#linux')
+	const remote_testing_checkbox = Selector('#remote-testing')
+	const submit_button = Selector('#submit-button')
+	const title= Selector('#article-header')
     await t
 	.maximizeWindow()
 	.setTestSpeed(0.1)
-        .typeText('#developer-name', 'Tammy') //enter dev name
-	.click('#remote-testing')//select remote testing checkbox
-	.click('#linux') //select linux radio button
-        .click('#submit-button') //click onsubmit
-        .expect(Selector('#article-header').innerText).eql('Thank you, Tammy!');
+        .typeText(developer_name, 'Tammy') //enter dev name
+	.click(remote_testing_checkbox)//select remote testing checkbox
+	.click(linux_os) //select linux radio button
+        .click(submit_button) //click onsubmit
+        .expect(Selector(title).innerText).eql('Thank you, Tammy!');
 });
 test('Second Test- negative scenario', async t => {
     await t
@@ -45,10 +50,6 @@ test('Skipped Test', async t => {
 	.maximizeWindow()
 	.setTestSpeed(0.1)
 	.typeText('#developer-name', 'Tammy') //enter dev name
-	.click('#linux')//selecte linux radio button
-	.click('#tried-test-cafe') //select tried test cafe checkbox
-	.click('#slider')
-    	.click('#submit-button')
-        .expect(Selector('#article-header').innerText).eql('Thank you, Tammy!');
+	.click('#preferred-interface')
 });
 
